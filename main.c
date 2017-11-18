dcf94fa5a18779d0b3832a1e399394247c2ba55a
Rafael Campos Nunes <rafaelnunes@engineer.com>
2017-11-17 23:19:59 -0200

    Initial commit for development files
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

---
+++ main.c
@@ -0,0 +1,62 @@
+/**
+ *                Universidade Tecnológica Federal do Paraná
+ *
+ * Copyright (c) 2017 Rafael Nunes <rafaelnunes@engineer.com>
+ * Copyright (c) 2017 Mikael Messias
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to
+ * deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ *
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "cmdline.h"
+#include "bfs.h"
+#include "bfs_string.h"
+
+int main(int argc, char **argv) {
+
+    FILE *fs = NULL;
+
+    if (argc > 1) {
+        char *fn = argv[1];
+
+        if (bfs_strncmpl(fn, ".txt", 3) == 1) {
+            fprintf(stderr, "VFS describe file has to have a .txt extension");
+            return 1;
+        }
+
+        fs = fopen(fn, "r");
+    } else {
+        fs = fopen("fs.txt", "r");
+    }
+
+    if (fs == NULL) {
+        return 1;
+    }
+
+    bfs_init();
+
+    // Load filesystem into memory.
+    // Current layout of the filesystem is still being discussed.
+    bfs_load(fs);
+
+    cmdline_start();
+
+    return 0;
+}
